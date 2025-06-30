8688a9eab2cc4db9d3d0a5477a066359a28c6c37
EDITz <7481152+EDITzDev@users.noreply.github.com>
2025-06-30 20:21:35 +0100

    Fix Feature Unlock Component

--- Servers/SkySaga.Game/Components/ClientFeatureUnlockComponent.cs
+++ Servers/SkySaga.Game/Components/ClientFeatureUnlockComponent.cs
@@ -1,26 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-
-using RakNet;
+﻿using RakNet;
 
 namespace SkySaga.Game.Components;
 
-public class ClientFeatureUnlockComponent : Component
+public class ClientFeatureUnlockComponent : FeatureUnlockComponent
 {
-    public List<bool> FeatureIsLockedStatusList { get; set { field = value; OnParameterChanged(); } } = [];
 
     public override bool TrySync(string parameterName, BitStream bitStream)
     {
-        if (parameterName.Equals(nameof(FeatureIsLockedStatusList), StringComparison.OrdinalIgnoreCase))
-        {
-            bitStream.Write0();
-
-            for (var i = 0; i < 30; i++)
-                bitStream.Write0();
-
-            return true;
-        }
-
-        return false;
+        return base.TrySync(parameterName, bitStream);
     }
 }
---
+++ Servers/SkySaga.Game/Components/FeatureUnlockComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using RakNet;
+
+namespace SkySaga.Game.Components;
+
+public class FeatureUnlockComponent : Component
+{
+    // public object FeatureUnlockRequirementList { get; set { field = value; OnParameterChanged(); } }
+    public List<bool> FeatureIsLockedStatusList { get; set { field = value; OnParameterChanged(); } } = [];
+
+    public override bool TrySync(string parameterName, BitStream bitStream)
+    {
+        if (parameterName.Equals(nameof(FeatureIsLockedStatusList), StringComparison.OrdinalIgnoreCase))
+        {
+            bitStream.Write0();
+
+            for (var i = 0; i < 30; i++)
+                bitStream.Write0();
+
+            return true;
+        }
+
+        return false;
+    }
+}
